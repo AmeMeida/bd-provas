319d5fb118b56186d89b2277a0110bc4f990cd96
AmeMeida <ame.pistache@gmail.com>
2024-03-27 16:25:25 -0300

    testando se o gh pages vai

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://amemeida.github.io/bd-provas",
+  base: "bd-provas"
+});
