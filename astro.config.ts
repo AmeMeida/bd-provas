9d077df01e7c4c00ccbed5f4c69c2e3cc2ee09ff
AmeMeida <ame.pistache@gmail.com>
2024-03-28 13:18:36 -0300

    rearranjando pastas e arrumando caminhos

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://amemeida.github.io/bd-provas",
+  base: "bd-provas/",
 });
